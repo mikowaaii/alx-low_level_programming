0b1777c98e72f97fc6047fbe4f22982df397a0c1
mikowaaii <mikoyan.julie@gmail.com>
2022-09-11 13:31:39 -0700

    3rd commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * puts - prints "Programming is like building a multilingual puzzle
+ *
+ * @str: string to be printed
+ *
+ * Return: printed string or 0
+ */
+char puts(char str)
+{
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
+	return 0;
+}
