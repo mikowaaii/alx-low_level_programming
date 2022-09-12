e772c5e1e306741ff70d490c537139dedf7babdc
mikowaaii <mikoyan.julie@gmail.com>
2022-09-12 15:16:25 -0700

    5th commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+		putchar(toupper(x));
+	}
+	putchar('\n');
+	return(0);
+}
