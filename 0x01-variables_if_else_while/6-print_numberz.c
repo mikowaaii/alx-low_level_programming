24abd1658bd18b0ce353cec72bf312d60422ff52
mikowaaii <mikoyan.julie@gmail.com>
2022-09-12 16:43:07 -0700

    8th commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
