8972fd3843f4d3540180a37f449e2f3e96ce77dc
mikowaaii <mikoyan.julie@gmail.com>
2022-09-12 02:18:14 -0700

    5th commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return - prints "with proper grammar, but the outcome is a piece of art," then 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
