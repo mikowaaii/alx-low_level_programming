0eb957bc18d201f132da4de0db7f1c28b2761653
mikowaaii <mikoyan.julie@gmail.com>
2022-09-12 17:07:24 -0700

    10th commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+	
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (y = 'a';y <= 'f'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
