ff1446848429d13bc7e5b77cb60984b078a959da
mikowaaii <mikoyan.julie@gmail.com>
2022-09-15 15:17:31 -0700

    correction

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,13 +4,13 @@
  * mul - multiplies two integers
  * @a: int to be multiplied
  * @b: int to be multiplied
- * Return: returns 0
+ *
+ * Return: returns multiplied value
  */
 int mul(int a, int b)
 {
 	int i;
-
 	i = (a * b);
 
-	return (0);
+	return (i);
 }
