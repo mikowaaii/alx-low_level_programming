c9d0dfeeb8dfd05d98428cc52363814dcde28044
mikowaaii <mikoyan.julie@gmail.com>
2022-09-16 14:17:45 -0700

    5th commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers 0 to 9 and a new line
+ *
+ * Return: returns void
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if ((c != 50) || (c != 52))
+		{
+			_putchar(c);
+		}
+		_putchar('\n')
+	}
+}
