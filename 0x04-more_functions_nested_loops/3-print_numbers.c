2bd5407b6599bcb304b550947ac0344f48d1f4ec
mikowaaii <mikoyan.julie@gmail.com>
2022-09-16 14:33:21 -0700

    correct script

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,13 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * print_numbers - prints the numbers 0 to 9 followed by a new line
  *
  * Return: returns void
  */
 void print_numbers(void)
 {
 	int c;
-	c = 0;
 
 	for (c = 48; c <= 57; c++)
 	{
