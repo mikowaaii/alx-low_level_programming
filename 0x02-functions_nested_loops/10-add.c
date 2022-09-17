f7559f64acefcd1e285a1aaa167b3e59879e04c9
mikowaaii <mikoyan.julie@gmail.com>
2022-09-17 06:30:10 -0700

    10th commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * add - finds sum of two integers
+ * @a: int to be added
+ * @b: int to be added
+ *
+ * Return: returns 0
+ */
+int add(int a, int b);
+{
+	int r;
+	r = (a + b);
+
+	if (r > 9)
+	{
+		_putchar((r / 10) + '0');
+		_putchar((r % 10) + '0');
+	}
+	else
+	{
+		_putchar((r % 10) + '0');
+	}
+	return (0);
+}
