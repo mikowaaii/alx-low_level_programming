6bd234b2516cee203319e3c7ee058cf02b0103ac
mikowaaii <mikoyan.julie@gmail.com>
2022-09-19 15:21:09 -0700

    8th commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: the pointer
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int x, m;
+
+	x = strlen(str);
+
+	if (x % 2 == 0)
+	{
+		m = (x / 2);
+	}
+	else
+	{
+		m = ((x - 1) / 2);
+	}
+
+	while (*(str + m) != '\0')
+	{
+		_putchar(*(str + m));
+		m++;
+	}
+	_putchar('\n');
+}
