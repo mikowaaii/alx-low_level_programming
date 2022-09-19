07e141947ad001451fa015382a04fc5fc02d614b
mikowaaii <mikoyan.julie@gmail.com>
2022-09-19 14:38:14 -0700

    7th commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string starting with the 1st character
+ * @str: the pointer
+ *
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int x;
+
+	while (*(str + x) != '\n')
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(*(str + x));
+			x++;
+		}
+		_putchar(*(str + x));
+	}
+	_putchar('\n');
+}
