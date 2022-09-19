30be38f6b78a65fd847b18bd209184b7fac4cf3e
mikowaaii <mikoyan.julie@gmail.com>
2022-09-19 11:07:30 -0700

    3rd commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the pointer
+ *
+ * Return: returns the length of the string
+ */
+int _strlen(char *s)
+{
+	int x;
+
+	while ( *s != '\0')
+	{
+		x++;
+		*s++;
+	}
+	return (x);
+}
