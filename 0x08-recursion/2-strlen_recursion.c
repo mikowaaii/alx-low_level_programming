19c5671741fac99db24571b65998cec4420862bb
mikowaaii <mikoyan.julie@gmail.com>
2022-09-28 14:08:13 -0700

    3rd commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to be printed
+ *
+ * Return: returns 0
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if ((*s != '\0'))
+	{
+		n++;
+		_strlen_recursion(s + 1);
+	}
+	return (n);
+	return (0);
