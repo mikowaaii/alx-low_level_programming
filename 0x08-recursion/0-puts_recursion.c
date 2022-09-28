3bd69b08f4e77896fb950b4b5d019032223765b6
mikowaaii <mikoyan.julie@gmail.com>
2022-09-28 10:17:33 -0700

    1st commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string then a new line
+ * @s: string to be printed
+ *
+ * Return: returns nothing
+ */
+void _puts_recursion(char *s);
+{
+	if (*s = '\0')
+	{
+		return;
+	}
+	_puts_recursion(s++);
+}
