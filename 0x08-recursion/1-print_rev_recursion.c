13c56737041a9beb5e6c79a70e6db9010adff7b4
mikowaaii <mikoyan.julie@gmail.com>
2022-09-28 12:59:10 -0700

    2nd commit

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,10 +8,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if ((*s == '\0'))
+	if ((*s != '\0'))
 	{
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
-		return;
 	}
-	_print_rev_recursion(s + 1);
 }
