8d5a56ea7a141049d92abd97682b1ca535465384
mikowaaii <mikoyan.julie@gmail.com>
2022-10-10 07:11:34 -0700

    Task 0

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+# **0x0E. C - Structures, typedef**
+
+## Description
+
+This project is about structs and typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - info about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
